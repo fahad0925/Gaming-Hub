acd7f1e132db8efc15d344e07e3ab354fa8b10c9
fahad0925 <fahadqwerty777>
2025-07-21 02:35:10 -0700

    gaming-hub

--- app/[gameIndex]/page.tsx
+++ app/[gameIndex]/page.tsx
@@ -11,47 +11,41 @@ const page = ({ params: { gameIndex } }: Props) => {
   let game = data[Number(gameIndex)];
   console.log(game);
   return (
-    <div className="max-w-7xl mx-auto text-white p-6 mt-10 space-y-8  ">
+    <div className="max-w-7xl mx-auto text-white p-6 mt-10 space-y-8 lg:max-w-7xl lg:mx-auto lg:text-white lg:p-6 lg:mt-10 lg:space-y-8">
       {/* ✅ Title Centered */}
-      <h1 className="text-5xl font-bold text-center underline mb-20 m-6">
+      <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-center underline mb-10 sm:mb-14 md:mb-20 m-4 sm:m-6 md:m-6 lg:text-5xl lg:mb-20 lg:m-6">
         {game.title}
       </h1>
 
       {/* ✅ Image + Details Flex Layout */}
-      <div className="flex flex-col md:flex-row rounded-3xl shadow-[0_0_20px_rgba(255,255,255,0.5)] overflow-hidden p-6">
+      <div className="flex flex-col md:flex-col lg:flex-row rounded-2xl sm:rounded-3xl shadow-[0_0_10px_rgba(255,255,255,0.3)] sm:shadow-[0_0_15px_rgba(255,255,255,0.4)] md:shadow-[0_0_20px_rgba(255,255,255,0.5)] overflow-hidden p-4 sm:p-6 md:p-8 lg:flex lg:rounded-3xl lg:shadow-[0_0_20px_rgba(255,255,255,0.5)] lg:overflow-hidden lg:p-6">
         {/* ✅ Left Side Image */}
         <img
           src={game.thumbnail}
           alt={game.title}
-          className="md:w-3xl  object-cover pr-10 rounded-4xl "
+          className="w-full md:w-4xl   object-cover mb-6 md:mb-0 md:pr-10  rounded-xl sm:rounded-2xl md:rounded-4xl lg:md:w-3xl lg:object-cover lg:pr-10 lg:rounded-4xl"
         />
 
         {/* ✅ Right Side Details */}
-        <div className="flex-1 p-6 space-y-5 text-xl font-semibold truncate bg-gradient-to-tr from-black via-zinc-900 to-black rounded-2xl ">
+        <div className="flex-1 p-4 sm:p-6 space-y-4 sm:space-y-5 md:text-lg md:mt-5 text-sm sm:text-xl font-semibold truncate bg-gradient-to-tr from-black via-zinc-900 to-black rounded-xl sm:rounded-2xl lg:flex-1 lg:p-6 lg:space-y-5 lg:text-xl lg:font-semibold lg:truncate lg:bg-gradient-to-tr lg:from-black lg:via-zinc-900 lg:to-black lg:rounded-2xl">
           <p>
             <span className="font-bold">Rating:</span> {game.id}
           </p>
-
           <p>
             <span className="font-bold">Platform:</span> {game.platform}
           </p>
-
           <p>
             <span className="font-bold">Publisher:</span> {game.publisher}
           </p>
-
           <p>
             <span className="font-bold">Release Date:</span> {game.release_date}
           </p>
-
           <p>
             <span className="font-bold">Developer:</span> {game.developer}
           </p>
-
           <p>
             <span className="font-bold">Genre:</span> {game.genre}
           </p>
-
           <p>
             <span className="font-bold">Link:</span>{" "}
             <a
@@ -66,7 +60,7 @@ const page = ({ params: { gameIndex } }: Props) => {
       </div>
 
       {/* ✅ Description Neeche Centered */}
-      <p className=" text-center underline max-w-3xl mx-auto p-15 text-xl">
+      <p className="text-center underline max-w-2xl sm:max-w-3xl mx-auto p-6 text-lg sm:text-xl lg:text-center lg:underline lg:max-w-3xl lg:mx-auto lg:p-15 lg:text-xl">
         "{game.short_description}"
       </p>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 import Particles from "./components/Particles";
 import { FaSearch } from "react-icons/fa";
 import { FaWindows, FaGlobe } from "react-icons/fa";
+import { FaPlaystation } from "react-icons/fa";
 
 type Deal = {
   title: string;
@@ -21,7 +22,7 @@ export default function Page() {
   const [selectedGenre, setSelectedGenre] = useState("");
   const [selectPlatForm, setSelectPlatForm] = useState("all");
   const [orderBy, setOrderBy] = useState("relevance");
-  // ✅ Common Filter Function
+  //  Common Filter Function
   const SearchAll = (
     platform = selectPlatForm,
     genre = selectedGenre,
@@ -29,19 +30,19 @@ export default function Page() {
     query = inputRef.current?.value.toLowerCase().trim() || ""
   ) => {
     let filtered = data;
-    // for platform
+    // platfrom
     if (platform !== "all" && platform !== "") {
       filtered = filtered.filter((game) =>
         game.platform.toLowerCase().includes(platform.toLowerCase())
       );
     }
-    // sidebar
+    // genre
     if (genre) {
       filtered = filtered.filter(
         (game) => game.genre.toLowerCase() === genre.toLowerCase()
       );
     }
-    // search
+    // query
     if (query !== "") {
       filtered = filtered.filter((game) =>
         game.title.toLowerCase().includes(query)
@@ -68,13 +69,13 @@ export default function Page() {
     SearchAll(selectPlatForm, genre);
   };
 
-  // Platform Select
+  //  Platform Select
   const PlatFormOption = (platform: string) => {
     setSelectPlatForm(platform);
     SearchAll(platform, selectedGenre);
   };
 
-  //  Unique Genres
+  // Unique Genres
   const uniqueGenres = data.filter(
     (item, index, self) =>
       index === self.findIndex((t) => t.genre === item.genre)
@@ -95,41 +96,51 @@ export default function Page() {
         alphaParticles={true}
         disableRotation={false}
       />
-      <div className="min-h-screen bg-gradient-to-tr from-black-950 via-blue-950 to-black-950">
+      <div style={{ width: "100%", height: "100%", position: "relative" }}>
         {/* Navbar */}
-        <div className="flex flex-col md:flex-row md:items-center py-3 px-4 border-b border-gray-700 fixed top-0 w-full z-50 bg-gradient-to-tr from-black-950 via-blue-950 to-black">
-          <div className="mb-2 md:mb-0">
-            <SiGamejolt className="w-10 h-10 text-purple-100" />
+        <div className="flex flex-row md:flex-row md:items-center py-1 pt-5 p-2  bg-gradient-to-tr from-black-950 via-blue-950 to-black border border-gray-700 fixed top-0 w-full z-50 gap-1 md:gap-0 h-20 lg:flex lg:py-3 lg:pl-3 lg:pr-5 lg:bg-gradient-to-tr lg:from-black-950 lg:via-blue-950 lg:to-black lg:border lg:border-gray-700 lg:fixed lg:top-0 lg:w-full lg:z-50 lg:gap-3 lg:md:gap-0 lg:h-20">
+          <div className="mb-2 md:mb-0 lg:mb-2 lg:md:mb-0">
+            <SiGamejolt className="w-10 h-10  text-purple-100 lg:w-10 lg:h-10 lg:mr-2 lg:text-purple-100" />
           </div>
-          <div className="relative w-full md:w-auto flex-grow">
-            <FaSearch className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-400 text-lg" />
+          <div className="relative w-full md:w-auto flex-grow lg:relative lg:w-full lg:md:w-auto lg:flex-grow">
+            <FaSearch className="absolute left-5 top-5.5 -translate-y-1/2 text-gray-400 text-lg lg:absolute lg:left-5 lg:top-1/2 lg:-translate-y-1/2 lg:text-gray-400 lg:text-lg" />
             <input
               ref={inputRef}
               onChange={Search}
               type="text"
               placeholder="Search Games..."
-              className="w-full p-2 pl-12 rounded-3xl border-2 border-gray-700 bg-gray-800 text-white placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-purple-800 hover:shadow-[0_0_15px_rgba(168,85,247,0.7)] transition"
+              className="w-full p-2 pl-12 pr-3 rounded-4xl border-2 border-gray-700 bg-gray-800 text-white placeholder-gray-400 focus:outline-none focus:border-purple-700 focus:ring-1 focus:ring-purple-800 hover:shadow-[0_0_15px_rgba(168,85,247,0.7)] transition-all duration-200 lg:w-full lg:p-2 lg:pl-12 lg:pr-3 lg:rounded-4xl lg:border-2 lg:border-gray-700 lg:bg-gray-800 lg:text-white lg:placeholder-gray-400 lg:focus:outline-none lg:focus:border-purple-700 lg:focus:ring-1 lg:focus:ring-purple-800 lg:hover:shadow-[0_0_15px_rgba(168,85,247,0.7)] lg:transition-all lg:duration-200"
             />
           </div>
         </div>
 
-        {/* col-6 Grid */}
-        <div className="grid grid-cols-1 lg:grid-cols-6 pt-24">
+        {/* Main Grid */}
+        <div className="grid grid-cols-1 md:grid-cols-12 pt-20 lg:grid lg:grid-cols-1 lg:md:grid-cols-6 lg:pt-20">
           {/* Sidebar */}
-          <div className="hidden lg:block col-span-1 bg-gradient-to-tr from-gray-800 via-slate-900 to-black p-4 text-white border-r border-gray-700 sticky top-24 h-[calc(100vh-6rem)] overflow-auto">
-            <ul className="space-y-4 text-md">
+          <div className="hidden md:block md:col-span-3 bg-gradient-to-tr from-gray-800 via-slate-900 to-black p-4 text-white border border-gray-700 sticky top-20 h-[calc(100vh-6rem)] overflow-auto lg:hidden lg:md:block lg:md:col-span-1 lg:bg-gradient-to-tr lg:from-gray-800 lg:via-slate-900 lg:to-black lg:p-4 lg:text-white lg:border lg:border-gray-700 lg:sticky lg:top-20 lg:h-[calc(100vh-6rem)] lg:overflow-auto">
+            <ul className="space-y-9 text-md lg:space-y-9 lg:text-md">
               <li
-                onClick={() => setSelectedGenre("")}
-                className="cursor-pointer hover:underline"
+                onClick={() => {
+                  setSelectedGenre("");
+                  SearchAll(selectPlatForm, "");
+                }}
+                className="cursor-pointer hover:underline text-white  text-xl flex lg:cursor-pointer lg:hover:underline lg:text-white lg:text-xl lg:flex"
               >
+                <FaPlaystation className="h-9 w-9 mr-3 lg:h-9 lg:w-9 lg:mr-3" />
                 All Genres
               </li>
+
               {uniqueGenres.map((game, i) => (
                 <li
-                  key={i}
-                  onClick={() => setSelectedGenre(game.genre)}
-                  className="cursor-pointer hover:underline"
+                  key={game.genre}
+                  onClick={() => handleGenreClick(game.genre)}
+                  className="cursor-pointer hover:underline text-white flex lg:cursor-pointer lg:hover:underline lg:text-white lg:flex"
                 >
+                  <img
+                    src={game.thumbnail}
+                    alt={game.title}
+                    className="w-9 h-9 rounded-xs mr-3 object-cover lg:w-9 lg:h-9 lg:rounded-xs lg:mr-3 lg:object-cover"
+                  />
                   {game.genre}
                 </li>
               ))}
@@ -137,63 +148,82 @@ export default function Page() {
           </div>
 
           {/* Main Content */}
-          <div className="col-span-1 lg:col-span-5 p-4">
-            <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-center md:text-left mb-4 text-white line-clamp-2">
-              {selectedGenre
-                ? `${selectedGenre} Games (${filteredData.length})`
-                : `All Games (${filteredData.length})`}
-            </h1>
+          <div className="md:col-span-9 w-fit lg:w-full bg-gradient-to-tr from-blue-950 via-slate-900 to-black-950 border border-gray-700 lg:md:col-span-5 lg:bg-gradient-to-tr lg:from-blue-950 lg:via-slate-900 lg:to-black-950 lg:border lg:border-gray-700">
+            <div className=" lg:flex lg:justify-between lg:flex-row flex flex-col justify-center items-center">
+              <div>
+                <h1 className="text-3xl font-bold pt-5 text-white lg:text-4xl  xl:text-5xl lg:font-bold lg:pl-10 lg:pt-5 lg:text-white ">
+                  {selectedGenre
+                    ? `${selectedGenre} Games (${filteredData.length})`
+                    : `All Games (${filteredData.length})`}
+                </h1>
+              </div>
 
-            <div className="flex flex-col sm:flex-row sm:justify-end gap-2 mb-4">
-              <select
-                value={orderBy}
-                onChange={(e) => setOrderBy(e.target.value)}
-                className="p-1 sm:p-2 text-sm sm:text-base bg-[#1f1f1f] text-gray-200 rounded-lg border border-gray-600"
-              >
-                <option value="relevance">Relevance</option>
-                <option value="id">ID</option>
-                <option value="name">Name</option>
-              </select>
-              <select
-                value={selectPlatForm}
-                onChange={(e) => setSelectPlatForm(e.target.value)}
-                className="p-1 sm:p-2 text-sm sm:text-base bg-[#1f1f1f] text-gray-200 rounded-lg border border-gray-600"
-              >
-                <option value="all">All Platforms</option>
-                <option value="pc">PC</option>
-                <option value="web">Web</option>
-              </select>
+              {/* Platform Selector */}
+              <div className="flex flex-row gap-4 lg:gap-0 ">
+                <select
+                  value={orderBy}
+                  onChange={(e) => setOrderBy(e.target.value)}
+                  className="bg-[#1f1f1f] text-gray-200 lg:h-10 lg:mt-5 font-medium border  mt-3 border-gray-600 rounded-xl p-2 w-45 shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 hover:border-blue-400 transition-all duration-200 ease-in-out cursor-pointer m-3,4 lg:bg-[#1f1f1f] lg:text-gray-200 lg:font-medium lg:border lg:border-gray-600 lg:rounded-xl lg:p-2 lg:w-3xs lg:shadow-md lg:focus:outline-none lg:focus:ring-2 lg:focus:ring-blue-500 lg:focus:border-blue-500 lg:hover:border-blue-400 lg:transition-all lg:duration-200 lg:ease-in-out lg:cursor-pointer lg:m-3,4  "
+                >
+                  <option value="relevance">Relevance</option>
+                  <option value="id">Id</option>
+                  <option value="name">Name</option>
+                </select>
+
+                <select
+                  value={selectPlatForm}
+                  onChange={(e) => PlatFormOption(e.target.value)}
+                  className="bg-[#1f1f1f] text-gray-200 lg:h-10 font-medium border lg:mt-5 mt-3  border-gray-600 rounded-xl p-2 w-45 shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 hover:border-blue-400 transition-all duration-200 ease-in-out cursor-pointer  lg:bg-[#1f1f1f] lg:text-gray-200 lg:font-medium lg:border lg:border-gray-600 lg:rounded-xl lg:p-2 lg:w-3xs lg:shadow-md lg:focus:outline-none lg:focus:ring-2 lg:focus:ring-blue-500 lg:focus:border-blue-500 lg:hover:border-blue-400 lg:transition-all lg:duration-200 lg:ease-in-out lg:cursor-pointer lg:m-5"
+                >
+                  <option value="all">All Platforms</option>
+                  <option value="pc">PC Games</option>
+                  <option value="web">Web Games</option>
+                </select>
+              </div>
             </div>
 
-            {/* Cards */}
-            <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            {/* Games Display */}
+            <div className="max-w-9xl p-4  lg:max-w-9xl lg:p-4">
               {filteredData.length === 0 ? (
-                <h2 className="text-white text-xl">No Game Found 😢</h2>
+                <h2 className="text-white text-3xl lg:text-white lg:text-3xl">
+                  No Game Found 😢
+                </h2>
               ) : (
-                filteredData.map((game, i) => (
-                  <Link key={i} href={`/${i}`}>
-                    <div className="bg-white/10 rounded-lg shadow-md backdrop-blur-md p-2 hover:scale-105 transform transition">
-                      <img
-                        src={game.thumbnail}
-                        alt={game.title}
-                        className="w-full h-40 md:h-60 object-cover rounded-md"
-                      />
-                      <div className="p-2">
-                        <h3 className="text-lg font-semibold text-white truncate">
-                          {game.title}
-                        </h3>
-                        <div className="flex justify-between items-center text-sm text-gray-300">
-                          <span>{game.genre}</span>
-                          {game.platform.toLowerCase().includes("pc") ? (
-                            <FaWindows />
-                          ) : (
-                            <FaGlobe />
-                          )}
+                <div className="grid gap-4  sm:grid-cols-2 md:grid-cols-2  xl:grid-cols-4  lg:grid lg:gap-4 md:gap-4 lg:grid-cols-1  lg:sm:grid-cols-2 lg:md:grid-cols-3 lg:xl:grid-cols-4">
+                  {filteredData.map((game, i) => (
+                    <Link key={i} href={`${i}`}>
+                      <div className="  bg-white/10 backdrop-blur-lg rounded-xl shadow-[0_0_20px_rgba(0,0,0,0.6)] border border-white/20 text-white overflow-hidden transform transition-all duration-300 hover:scale-105 hover:border-purple-700 hover:shadow-[0_0_22px_rgba(168,85,247,0.7)] lg:bg-white/10 lg:backdrop-blur-lg lg:rounded-lg lg:shadow-[0_0_20px_rgba(0,0,0,0.6)] lg:border lg:border-white/20 lg:text-white lg:overflow-hidden lg:transform lg:transition-all lg:duration-300 lg:hover:scale-105 lg:hover:border-purple-700 lg:hover:shadow-[0_0_22px_rgba(168,85,247,0.7)]    sm:max-h-60 lg:max-h-62 xl:max-h-89">
+                        <img
+                          src={game.thumbnail}
+                          alt={game.title}
+                          className="w-full h-60  sm:h-40 sm:object-cover object-cover lg:h-42  lg:w-full xl:h-60 lg:object-cover xl:object-cover"
+                        />
+                        <div className="p-4 lg:p-4 sm:p-2">
+                          <div className="flex items-center justify-between lg:flex lg:items-center lg:justify-between">
+                            <h3 className="text-lg sm:text-sm font-semibold truncate lg:text-lg lg:font-semibold lg:truncate">
+                              {game.title}
+                            </h3>
+                            <span className="text-sm  text-gray-500 bg-green-100 rounded-b-md py-1 px-2 lg:text-sm lg:text-gray-500 lg:bg-green-100 lg:rounded-b-md lg:py-1 lg:px-2">
+                              {game.id}
+                            </span>
+                          </div>
+                          <div className="flex gap-2 lg:flex lg:gap-2">
+                            <p className="text-sm sm:text-[11px] text-gray-300 lg:text-sm lg:text-gray-300">
+                              {game.genre}
+                            </p>
+                            <p>
+                              {game.platform.toLowerCase().includes("pc") ? (
+                                <FaWindows className="text-gray-400 pt-1 lg:w-4 sm:pt-0 sm:w-2  lg:text-gray-400 lg:pt-1" />
+                              ) : (
+                                <FaGlobe className="pt-0.5 lg:pt-0.5 lg:w-4 sm:w-2 sm:pt-0" />
+                              )}
+                            </p>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  </Link>
-                ))
+                    </Link>
+                  ))}
+                </div>
               )}
             </div>
           </div>
